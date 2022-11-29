762009218330ccd8487177db8303b0fb430b98d9
Moe Coovadia <coovadia.moe@gmail.com>
2022-11-29 03:16:23 -0500

    added client front-end

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>EMAILY</h1>
+      Collect feeback for your users!
+    </div>
+  );
+};
+
+export default Landing;
