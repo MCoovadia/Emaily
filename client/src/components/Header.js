3ee77a5a0ca0239bfc307f048fd25f057cc997f6
Moe Coovadia <coovadia.moe@gmail.com>
2022-12-02 19:26:43 -0500

    added billing and client side app!

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import Payments from "./Payments";
 
 class Header extends Component {
   renderContent() {
@@ -10,15 +11,26 @@ class Header extends Component {
       case false:
         return (
           <li>
-            <a href="/auth/google">Login with Google</a>
+            <a href="/auth/google">Login With Google</a>
           </li>
         );
       default:
-        return (
-          <li>
-            <a href="/api/logout">Logout</a>
-          </li>
-        );
+        return [
+          <div>
+            <li key="1">
+              <Payments />
+            </li>
+            ,
+            <li key="3" style={{ margin: "0 10px" }}>
+              Credits:{this.props.auth.credits}
+            </li>
+            ,
+            <li key="2">
+              <a href="/api/logout">Logout</a>
+            </li>
+            ,
+          </div>,
+        ];
     }
   }
 
@@ -43,4 +55,4 @@ function mapStateToProps({ auth }) {
   return { auth };
 }
 
-export default connect()(Header);
+export default connect(mapStateToProps)(Header);
